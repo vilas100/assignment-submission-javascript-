a594bd5d264109b30f96c0971263d6052b940e26
vilas100 <68494226+vilas100@users.noreply.github.com>
2021-09-10 14:26:17 +0530

    assignment day 3 submitted

---
+++ assignment 3/scheduler.js
@@ -0,0 +1,9 @@
+function addTask(){
+     let data = document.getElementById('inp').value;
+	 document.getElementById('two').innerHTML+=`<h1> class='todo'>${data}</h1>);
+	 document.getElementById('inp').value="";
+	 }
+	
+function changeColor(){
+   document.getElementById('two').style.backgroundColor="orangered";
+   }
